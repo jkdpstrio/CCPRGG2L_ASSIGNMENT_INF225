c737a043cc76a436a1c7fb2248104cad969abaa5
Kenneeeeeeeth <120693762+jkdpstrio@users.noreply.github.com>
2023-02-10 10:09:19 +0800

    Create Person.java

---
+++ Exercise 5/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+
+    private String name;
+    private int ccnumber;
+
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String newName) {
+
+        this.name = newName;
+    }
+
+    public int getNum() {
+        return this.ccnumber;
+    }
+
+    public void setNum(int ccnumber) {
+        this.ccnumber = ccnumber;
+    }
+
+
+}
