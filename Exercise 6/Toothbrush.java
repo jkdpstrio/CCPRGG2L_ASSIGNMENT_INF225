b4231ee1050351dbc5ee85f99ae833c55bc058f1
Kenneeeeeeeth <120693762+jkdpstrio@users.noreply.github.com>
2023-02-10 11:42:10 +0800

    Create Toothbrush.java

---
+++ Exercise 6/Toothbrush.java
@@ -0,0 +1,10 @@
+public class Toothbrush extends Groceryitem {
+
+    public void showItemName() {
+        System.out.println(this.name + " is a Toothbrush");
+    }
+    public void showPrice(Groceryitem Item) {
+        System.out.println("Price " + this.price);
+    }
+    
+}
