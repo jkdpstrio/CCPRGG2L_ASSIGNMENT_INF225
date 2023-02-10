e145bf057c188b4a5bddb37b68c45a5a50c1083a
Kenneeeeeeeth <120693762+jkdpstrio@users.noreply.github.com>
2023-02-10 11:42:54 +0800

    Create Toothpaste.java

---
+++ Exercise 6/Toothpaste.java
@@ -0,0 +1,10 @@
+public class Toothpaste extends Groceryitem{
+
+    public void showItemName() {
+        System.out.println(this.name + " is a Toothpaste");
+    }
+    public void showPrice(Groceryitem Item) {
+        System.out.println("Price " + this.price);
+    }
+    
+}
