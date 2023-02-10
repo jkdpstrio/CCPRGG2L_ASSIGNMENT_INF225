b7d4982bd4568ea7b7b067e2d5346ffb89ab3f20
Kenneeeeeeeth <120693762+jkdpstrio@users.noreply.github.com>
2023-02-10 11:44:27 +0800

    Create Animal.java

---
+++ Exercise 6/Animal.java
@@ -0,0 +1,10 @@
+public class Lion implements Animal{
+
+    public void walk(){
+        System.out.println("Lion is walking");
+    }
+    public void run(){
+        System.out.println("Lion is running");
+    }
+    
+}
