00aed41cfad1bdbcaa5aacb21f245acbd9606405
Kenneeeeeeeth <120693762+jkdpstrio@users.noreply.github.com>
2023-03-19 20:09:11 +0800

    Create ColorMenu.java

---
+++ Exercise 7/ColorMenu.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+    public class ColorMenu extends JFrame {
+
+        private String[] colorNameArray = {"GRAY", "LIGHT GRAY","WHITE", "BLACK"};
+        private Color []colorListArray= {Color.gray,Color.lightGray,Color.white, Color.BLACK};
+        JList colorList;
+    
+            ColorMenu(){
+
+                JLabel label = new JLabel();
+                label.setText("Select Color");
+                label.setFont(new Font("DialogInput", Font.BOLD,30));
+                this.add(label);
+
+                colorList = new JList(colorNameArray);
+                colorList.setFont(new Font("DialogInput",Font.BOLD,30));
+                colorList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+                this.add(colorList);
+        
+                EventHandler handler = new EventHandler();
+                colorList.addListSelectionListener(handler);
+
+                this.setLayout(new FlowLayout());
+                this.setTitle("Snake Game");
+                this.setDefaultCloseOperation(EXIT_ON_CLOSE);           
+                this.setSize(330,330);           
+                this.setLocationRelativeTo(null);
+                this.setVisible(true);
+                this.setResizable(false);
+                this.getContentPane().setBackground(Color.BLACK);
+    
+    }
+    
+    private class EventHandler implements ListSelectionListener{
+
+        public void valueChanged(ListSelectionEvent event){
+
+            GamePanel gp = new GamePanel();
+            gp.gameBGC = colorListArray[colorList.getSelectedIndex()];
+
+            new GameFrame();
+            dispose();
+        }
+    }  
+}
